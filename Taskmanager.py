1aedc6ec8064bc759199d998c246bca87fa8d9db
unknown <rhiki.080bct033@tcioe.edu.np>
2025-07-21 11:11:06 +0545

    Implemented Running Tasks, ascending order

--- Taskmanager.py
+++ Taskmanager.py
@@ -10,15 +10,12 @@ from PyQt5.QtWidgets import (
     QLineEdit, QWidget, QPushButton, QSpinBox, 
     QMessageBox, QToolBar, QMessageBox,QTreeWidget,
     QTreeWidgetItem,QWidget,QVBoxLayout,QHBoxLayout,
-    QLabel,QGridLayout
+    QLabel,QGridLayout,QInputDialog
 )
 import pyqtgraph as pg
 class TaskManager:
     def __init__(self):
         self.tasks = []
-        self.total_proc=0
-        self.totalmem_per = 0
-        self.totalcpu = 0
 
     def processes(self):
         num = 1
@@ -29,10 +26,14 @@ class TaskManager:
                 continue
             memory = round((proc.memory_info().rss)/(1024*1024),3)
             cpu = proc.cpu_percent(interval = 0)/psutil.cpu_count()
-            self.process = Tasks(num,name,pid,memory,cpu)
-            self.total_proc+=1
+            self.process = Tasks(0,name,pid,memory,cpu)
             self.tasks.append(self.process.stpr())
+        self.tasks.sort(key = lambda x:x['Process'].lower())
+        num = 1
+        for task in self.tasks:
+            task['S.N.'] = num
             num+=1
+            
         
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -48,7 +49,8 @@ class MainWindow(QMainWindow):
         self.performance_layout = QVBoxLayout()
         self.CPU_label = QLabel(f"Total CPU Usage: {str(psutil.cpu_percent())}%")
         self.memory_label = QLabel(f"Total Memory Usage: {str(psutil.virtual_memory().percent)}%")
-        self.new_task_button = QPushButton("Run New Task")
+        self.new_task_button = QPushButton("Run New Process")
+        self.new_task_button.clicked.connect(self.run_task)
         self.performance_layout.addWidget(self.CPU_label)
         self.performance_layout.addWidget(self.memory_label)
         self.performance_layout.addWidget(self.new_task_button)
@@ -62,8 +64,8 @@ class MainWindow(QMainWindow):
         self.main_layout.addWidget(self.tree,stretch=4)
         #Buttons
         self.button_layout = QHBoxLayout()
-        self.refresh_task = QPushButton("Refresh Tasks")
-        self.end_task = QPushButton("End Task")
+        self.refresh_task = QPushButton("Refresh Process List")
+        self.end_task = QPushButton("End Process")
         self.force_end = QPushButton("Force End")
         self.button_layout.addWidget(self.end_task)
         self.button_layout.addWidget(self.refresh_task)
@@ -84,15 +86,15 @@ class MainWindow(QMainWindow):
         self.graph_layout.addWidget(self.memory_plot,0,0)
         self.graph_layout.addWidget(self.cpu_plot,0,2)
         self.main_layout.addWidget(self.graph_widget,stretch=3)
-    
-
 
     def fill_processes(self):
         self.T = TaskManager()
         self.T.processes()
         self.tree.setColumnCount(5)
-        self.tree.clear()
         headerlabels = self.T.tasks[0].keys()
+        root = self.tree.invisibleRootItem()
+        while (root.childCount() > 0):
+            root.removeChild(root.child(0))
         self.tree.setHeaderLabels(headerlabels)
         for task in self.T.tasks:
             task_item = QTreeWidgetItem(self.tree)
@@ -106,15 +108,40 @@ class MainWindow(QMainWindow):
     def on_button_click(self):
         self.fill_processes()
     
+    def run_task(self):
+        input_box = QInputDialog()
+        cmd,ok = input_box.getText(self,"Run New Process","Enter Process:")
+        if ok and cmd.strip():
+            try:
+                subprocess.Popen([cmd.strip()])
+                time.sleep(0.2)
+                self.fill_processes()
+            except FileNotFoundError:
+                QMessageBox.information(
+                    self,
+                    'Error',
+                    'No Such Program Found.'
+                )
+            except Exception as e:
+                QMessageBox.information(
+                    self,
+                    'Error',
+                    'Cant Open Proess.'
+                )
+
     def kill_task(self):
         if hasattr(self,'id'):
             try:
                 proc = psutil.Process(self.id)
                 proc.terminate()
-                time.sleep(0.1)
+                time.sleep(0.2)
                 self.fill_processes()
             except Exception as e:
-                print("Failed to end process",e)
+                QMessageBox.information(
+                    self,
+                    'Error',
+                    'Cannot End Process.'
+                )
         else:
            QMessageBox.information(
                self,
@@ -127,10 +154,15 @@ class MainWindow(QMainWindow):
             try:
                 proc = psutil.Process(self.id)
                 proc.kill()
-                time.sleep(0.1)
+                time.sleep(0.2)
                 self.fill_processes()
+
             except Exception as e:
-                print("Failed to kill process",e)
+                QMessageBox.information(
+                    self,
+                    'Error',
+                    'Cannot End Process.'
+                    )
         else:
            QMessageBox.information(
                self,
@@ -146,6 +178,7 @@ class MainWindow(QMainWindow):
         plot.getPlotItem().getAxis('bottom').setPen('k')
         plot.plot(pen=pg.mkPen(color,width = 2))
         return plot
+    
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
